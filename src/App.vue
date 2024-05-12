652db26ee450f505670796e63186bd20ca02d0c9
alnoorimustafa <alnoorimustafa@gmail.com>
2024-05-12 10:39:18 +0300

    s

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <script setup lang="ts">
-import { useTheme } from 'vuetify'
-import ScrollToTop from '@core/components/ScrollToTop.vue'
-import initCore from '@core/initCore'
-import { initConfigStore, useConfigStore } from '@core/stores/config'
-import { hexToRgb } from '@layouts/utils'
+  import { useTheme } from 'vuetify'
+  import ScrollToTop from '@core/components/ScrollToTop.vue'
+  import initCore from '@core/initCore'
+  import { initConfigStore, useConfigStore } from '@core/stores/config'
+  import { hexToRgb } from '@layouts/utils'
 
-const { global } = useTheme()
+  const { global } = useTheme()
 
-// ℹ️ Sync current theme with initial loader theme
-initCore()
-initConfigStore()
+  // ℹ️ Sync current theme with initial loader theme
+  initCore()
+  initConfigStore()
 
-const configStore = useConfigStore()
+  const configStore = useConfigStore()
 </script>
 
 <template>
   <VLocaleProvider :rtl="configStore.isAppRTL">
     <!-- ℹ️ This is required to set the background color of active nav link based on currently active global theme's primary -->
-    <VApp :style="`--v-global-theme-primary: ${hexToRgb(global.current.value.colors.primary)}`">
+    <VApp
+      :style="`--v-global-theme-primary: ${hexToRgb(
+        global.current.value.colors.primary
+      )}`"
+    >
       <RouterView />
       <ScrollToTop />
     </VApp>
--- src/plugins/1.router/index.ts
+++ src/plugins/1.router/index.ts
@@ -36,15 +36,6 @@ const router = createRouter({
   ],
 })
 
-router.onError((error, to) => {
-  if (
-    error.message.includes('Failed to fetch dynamically imported module') ||
-    error.message.includes('Importing a module script failed')
-  ) {
-    window.location = to.fullPath
-  }
-})
-
 setupGuards(router)
 
 export { router }
