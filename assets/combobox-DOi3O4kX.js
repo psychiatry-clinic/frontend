95ae725ce3ec2f1796b83d4de1b764b1bed955d8
mustafa-laptop <alnoorimustafa@gmail.com>
2024-05-16 00:07:55 +0300

    Updates

---
+++ assets/combobox-DOi3O4kX.js
@@ -0,0 +1,446 @@
+import{_ as h,V as _}from"./AppCombobox.vue_vue_type_script_setup_true_lang-CHSrFT4p.js";import{d as b,r as u,o as V,g,n as r,aX as d,w as P,b0 as A,f as o,b as l,v as i,e as n,x as y}from"./index-BkoIr6yh.js";import{a as D,b as w}from"./VList-CvvyJ3Y3.js";import{a as p,V as v}from"./VRow-D85Ewhef.js";import{V as U}from"./VChip-HOpiRy-7.js";import{V as L}from"./VAvatar-CivDSueD.js";import{_ as k}from"./AppCardCode.vue_vue_type_style_index_0_lang-CBChKl7R.js";import"./form-CJv9Io97.js";import"./VSelect-BMkNHuyW.js";import"./VTextField-C2gSdMZL.js";/* empty css                   */import"./VCounter-CBD31Zsb.js";import"./index-Ds37CoIQ.js";import"./index-NnWhEr9J.js";import"./VField-CRHXbthh.js";import"./easing-CjukEv2V.js";import"./VInput-Bn4cjs5o.js";import"./forwardRefs-C-GTDzx5.js";import"./dialog-transition-BWJ0TDCH.js";import"./VMenu-CNYWrpFA.js";import"./VOverlay-Dx-gzjuP.js";import"./lazy-BdnWOK3a.js";import"./scopeId-DB_d-rAg.js";import"./VCheckboxBtn-B6KL9PVT.js";import"./VSelectionControl-Bt0dhks5.js";import"./filter-C3316zaz.js";import"./ssrBoot-BGlu6d5Z.js";import"./createSimpleFunctional-Bs0Jn_G0.js";import"./VDivider-Dq5etA6h.js";/* empty css              */import"./VSlideGroup-_w52RLV9.js";import"./VImg-CowEZo-Q.js";import"./vue3-perfect-scrollbar.esm-bQtCWrKj.js";import"./VCard-Dmj-2I2I.js";import"./VCardText-xMOIUJ2c.js";const R=b({__name:"DemoComboboxClearable",setup(f){const e=u(["Vuetify","Programming"]),s=["Programming","Design","Vue","Vuetify"];return(c,t)=>{const m=h;return V(),g(m,{modelValue:r(e),"onUpdate:modelValue":t[0]||(t[0]=a=>d(e)?e.value=a:null),items:s,label:"Combobox",multiple:"",placeholder:"deployment",clearable:""},null,8,["modelValue"])}}}),N=n("kbd",null,"enter",-1),T=b({__name:"DemoComboboxNoDataWithChips",setup(f){const e=["Gaming","Programming","Vue","Vuetify"],s=u(["Vuetify"]),c=u(null);return P(s,t=>{t.length>5&&A(()=>s.value.pop())}),(t,m)=>{const a=h;return V(),g(a,{modelValue:r(s),"onUpdate:modelValue":m[0]||(m[0]=C=>d(s)?s.value=C:null),"search-input":r(c),"onUpdate:searchInput":m[1]||(m[1]=C=>d(c)?c.value=C:null),items:e,"hide-selected":"","hide-no-data":!1,placeholder:"deployment",hint:"Maximum of 5 tags",label:"Add some tags",multiple:"","persistent-hint":""},{"no-data":o(()=>[l(D,null,{default:o(()=>[l(w,null,{default:o(()=>[i(' No results matching "'),n("strong",null,y(r(c)),1),i('". Press '),N,i(" to create a new one ")]),_:1})]),_:1})]),_:1},8,["modelValue","search-input"])}}}),$=b({__name:"DemoComboboxMultiple",setup(f){const e=u(["Vuetify","Programming"]),s=["Programming","Design","Vue","Vuetify"];return(c,t)=>{const m=h;return V(),g(v,null,{default:o(()=>[l(p,{cols:"12"},{default:o(()=>[l(m,{modelValue:r(e),"onUpdate:modelValue":t[0]||(t[0]=a=>d(e)?e.value=a:null),items:s,placeholder:"deployment",label:"Select a favorite activity or create a new one",multiple:""},null,8,["modelValue"])]),_:1}),l(p,{cols:"12"},{default:o(()=>[l(m,{modelValue:r(e),"onUpdate:modelValue":t[1]||(t[1]=a=>d(e)?e.value=a:null),items:s,placeholder:"deployment",label:"I use chips",multiple:"",chips:""},null,8,["modelValue"])]),_:1}),l(p,{cols:"12"},{default:o(()=>[l(m,{modelValue:r(e),"onUpdate:modelValue":t[2]||(t[2]=a=>d(e)?e.value=a:null),placeholder:"deployment",label:"I'm readonly",chips:"",multiple:"",readonly:""},null,8,["modelValue"])]),_:1}),l(p,{cols:"12"},{default:o(()=>[l(m,{modelValue:r(e),"onUpdate:modelValue":t[3]||(t[3]=a=>d(e)?e.value=a:null),items:s,placeholder:"deployment",label:"I use selection slot",multiple:""},{selection:o(({item:a})=>[l(U,{size:"small"},{prepend:o(()=>[l(L,{start:"",color:"primary",size:"16"},{default:o(()=>[i(y(String(a.title).charAt(0).toUpperCase()),1)]),_:2},1024)]),default:o(()=>[i(" "+y(a.title),1)]),_:2},1024)]),_:1},8,["modelValue"])]),_:1})]),_:1})}}}),S=b({__name:"DemoComboboxVariant",setup(f){const e=u(["Programming"]),s=["Programming","Design","Vue","Vuetify"];return(c,t)=>(V(),g(v,null,{default:o(()=>[l(p,{cols:"12"},{default:o(()=>[l(_,{modelValue:r(e),"onUpdate:modelValue":t[0]||(t[0]=m=>d(e)?e.value=m:null),items:s,multiple:"",placeholder:"deployment",variant:"solo",label:"solo"},null,8,["modelValue"])]),_:1}),l(p,{cols:"12"},{default:o(()=>[l(_,{modelValue:r(e),"onUpdate:modelValue":t[1]||(t[1]=m=>d(e)?e.value=m:null),multiple:"",items:s,placeholder:"deployment",variant:"outlined",label:"Outlined"},null,8,["modelValue"])]),_:1}),l(p,{cols:"12"},{default:o(()=>[l(_,{modelValue:r(e),"onUpdate:modelValue":t[2]||(t[2]=m=>d(e)?e.value=m:null),multiple:"",items:s,placeholder:"deployment",variant:"underlined",label:"Underlined"},null,8,["modelValue"])]),_:1}),l(p,{cols:"12"},{default:o(()=>[l(_,{modelValue:r(e),"onUpdate:modelValue":t[3]||(t[3]=m=>d(e)?e.value=m:null),multiple:"",items:s,placeholder:"deployment",variant:"filled",label:"Filled"},null,8,["modelValue"])]),_:1}),l(p,{cols:"12"},{default:o(()=>[l(_,{modelValue:r(e),"onUpdate:modelValue":t[4]||(t[4]=m=>d(e)?e.value=m:null),multiple:"",items:s,variant:"plain",placeholder:"deployment",label:"Plain"},null,8,["modelValue"])]),_:1})]),_:1}))}}),j=b({__name:"DemoComboboxDensity",setup(f){const e=u(["Vuetify","Programming"]),s=["Programming","Design","Vue","Vuetify"];return(c,t)=>{const m=h;return V(),g(m,{modelValue:r(e),"onUpdate:modelValue":t[0]||(t[0]=a=>d(e)?e.value=a:null),items:s,label:"Combobox",density:"compact",placeholder:"deployment",multiple:""},null,8,["modelValue"])}}}),z=b({__name:"DemoComboboxBasic",setup(f){const e=u("Programming"),s=["Programming","Design","Vue","Vuetify"];return(c,t)=>{const m=h;return V(),g(m,{modelValue:r(e),"onUpdate:modelValue":t[0]||(t[0]=a=>d(e)?e.value=a:null),items:s,placeholder:"deployment"},null,8,["modelValue"])}}}),B={ts:`<script lang="ts" setup>
+const selectedItem = ref('Programming')
+const items = ['Programming', 'Design', 'Vue', 'Vuetify']
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="selectedItem"
+    :items="items"
+    placeholder="deployment"
+  />
+</template>
+`,js:`<script setup>
+const selectedItem = ref('Programming')
+
+const items = [
+  'Programming',
+  'Design',
+  'Vue',
+  'Vuetify',
+]
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="selectedItem"
+    :items="items"
+    placeholder="deployment"
+  />
+</template>
+`},M={ts:`<script lang="ts" setup>
+const select = ref(['Vuetify', 'Programming'])
+const items = ['Programming', 'Design', 'Vue', 'Vuetify']
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="select"
+    :items="items"
+    label="Combobox"
+    multiple
+    placeholder="deployment"
+    clearable
+  />
+</template>
+`,js:`<script setup>
+const select = ref([
+  'Vuetify',
+  'Programming',
+])
+
+const items = [
+  'Programming',
+  'Design',
+  'Vue',
+  'Vuetify',
+]
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="select"
+    :items="items"
+    label="Combobox"
+    multiple
+    placeholder="deployment"
+    clearable
+  />
+</template>
+`},W={ts:`<script lang="ts" setup>
+const select = ref(['Vuetify', 'Programming'])
+const items = ['Programming', 'Design', 'Vue', 'Vuetify']
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="select"
+    :items="items"
+    label="Combobox"
+    density="compact"
+    placeholder="deployment"
+    multiple
+  />
+</template>
+`,js:`<script setup>
+const select = ref([
+  'Vuetify',
+  'Programming',
+])
+
+const items = [
+  'Programming',
+  'Design',
+  'Vue',
+  'Vuetify',
+]
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="select"
+    :items="items"
+    label="Combobox"
+    density="compact"
+    placeholder="deployment"
+    multiple
+  />
+</template>
+`},F={ts:`<script lang="ts" setup>
+const selectedItem = ref(['Vuetify', 'Programming'])
+const items = ['Programming', 'Design', 'Vue', 'Vuetify']
+<\/script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        :items="items"
+        placeholder="deployment"
+        label="Select a favorite activity or create a new one"
+        multiple
+      />
+    </VCol>
+
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        :items="items"
+        placeholder="deployment"
+        label="I use chips"
+        multiple
+        chips
+      />
+    </VCol>
+
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        placeholder="deployment"
+        label="I'm readonly"
+        chips
+        multiple
+        readonly
+      />
+    </VCol>
+
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        :items="items"
+        placeholder="deployment"
+        label="I use selection slot"
+        multiple
+      >
+        <template #selection="{ item }">
+          <VChip size="small">
+            <template #prepend>
+              <VAvatar
+                start
+                color="primary"
+                size="16"
+              >
+                {{ String(item.title).charAt(0).toUpperCase() }}
+              </VAvatar>
+            </template>
+
+            {{ item.title }}
+          </VChip>
+        </template>
+      </AppCombobox>
+    </VCol>
+  </VRow>
+</template>
+`,js:`<script setup>
+const selectedItem = ref([
+  'Vuetify',
+  'Programming',
+])
+
+const items = [
+  'Programming',
+  'Design',
+  'Vue',
+  'Vuetify',
+]
+<\/script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        :items="items"
+        placeholder="deployment"
+        label="Select a favorite activity or create a new one"
+        multiple
+      />
+    </VCol>
+
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        :items="items"
+        placeholder="deployment"
+        label="I use chips"
+        multiple
+        chips
+      />
+    </VCol>
+
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        placeholder="deployment"
+        label="I'm readonly"
+        chips
+        multiple
+        readonly
+      />
+    </VCol>
+
+    <VCol cols="12">
+      <AppCombobox
+        v-model="selectedItem"
+        :items="items"
+        placeholder="deployment"
+        label="I use selection slot"
+        multiple
+      >
+        <template #selection="{ item }">
+          <VChip size="small">
+            <template #prepend>
+              <VAvatar
+                start
+                color="primary"
+                size="16"
+              >
+                {{ String(item.title).charAt(0).toUpperCase() }}
+              </VAvatar>
+            </template>
+
+            {{ item.title }}
+          </VChip>
+        </template>
+      </AppCombobox>
+    </VCol>
+  </VRow>
+</template>
+`},G={ts:`<script lang="ts" setup>
+const items = ['Gaming', 'Programming', 'Vue', 'Vuetify']
+const selectedList = ref(['Vuetify'])
+const search = ref(null)
+
+watch(selectedList, value => {
+  if (value.length > 5)
+    nextTick(() => selectedList.value.pop())
+})
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="selectedList"
+    v-model:search-input="search"
+    :items="items"
+    hide-selected
+    :hide-no-data="false"
+    placeholder="deployment"
+    hint="Maximum of 5 tags"
+    label="Add some tags"
+    multiple
+    persistent-hint
+  >
+    <template #no-data>
+      <VListItem>
+        <VListItemTitle>
+          No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+        </VListItemTitle>
+      </VListItem>
+    </template>
+  </AppCombobox>
+</template>
+`,js:`<script setup>
+const items = [
+  'Gaming',
+  'Programming',
+  'Vue',
+  'Vuetify',
+]
+
+const selectedList = ref(['Vuetify'])
+const search = ref(null)
+
+watch(selectedList, value => {
+  if (value.length > 5)
+    nextTick(() => selectedList.value.pop())
+})
+<\/script>
+
+<template>
+  <AppCombobox
+    v-model="selectedList"
+    v-model:search-input="search"
+    :items="items"
+    hide-selected
+    :hide-no-data="false"
+    placeholder="deployment"
+    hint="Maximum of 5 tags"
+    label="Add some tags"
+    multiple
+    persistent-hint
+  >
+    <template #no-data>
+      <VListItem>
+        <VListItemTitle>
+          No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+        </VListItemTitle>
+      </VListItem>
+    </template>
+  </AppCombobox>
+</template>
+`},O={ts:`<script lang="ts" setup>
+const selectedItem = ref(['Programming'])
+const items = ['Programming', 'Design', 'Vue', 'Vuetify']
+<\/script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        :items="items"
+        multiple
+        placeholder="deployment"
+        variant="solo"
+        label="solo"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        placeholder="deployment"
+        variant="outlined"
+        label="Outlined"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        placeholder="deployment"
+        variant="underlined"
+        label="Underlined"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        placeholder="deployment"
+        variant="filled"
+        label="Filled"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        variant="plain"
+        placeholder="deployment"
+        label="Plain"
+      />
+    </VCol>
+  </VRow>
+</template>
+`,js:`<script setup>
+const selectedItem = ref(['Programming'])
+
+const items = [
+  'Programming',
+  'Design',
+  'Vue',
+  'Vuetify',
+]
+<\/script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        :items="items"
+        multiple
+        placeholder="deployment"
+        variant="solo"
+        label="solo"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        placeholder="deployment"
+        variant="outlined"
+        label="Outlined"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        placeholder="deployment"
+        variant="underlined"
+        label="Underlined"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        placeholder="deployment"
+        variant="filled"
+        label="Filled"
+      />
+    </VCol>
+    <VCol cols="12">
+      <VCombobox
+        v-model="selectedItem"
+        multiple
+        :items="items"
+        variant="plain"
+        placeholder="deployment"
+        label="Plain"
+      />
+    </VCol>
+  </VRow>
+</template>
+`},X=n("p",null,"With Combobox, you can allow a user to create new values that may not be present in a provided items list.",-1),Y=n("p",null,[i(" You can use "),n("code",null,"Density"),i(" prop to reduce combobox height and lower max height of list items. Available options are: "),n("code",null,"default"),i(", "),n("code",null,"comfortable"),i(", and "),n("code",null,"compact"),i(". ")],-1),q=n("p",null,[i("Use "),n("code",null,"solo"),i(", "),n("code",null,"outlined"),i(", "),n("code",null,"underlined"),i(", "),n("code",null,"filled"),i(" and "),n("code",null,"plain"),i(" options of "),n("code",null,"variant"),i(" prop to change the look of combobox. ")],-1),E=n("p",null,"Previously known as tags - user is allowed to enter more than 1 value",-1),H=n("p",null,"Previously known as tags - user is allowed to enter more than 1 value",-1),J=n("p",null,[i("Use "),n("code",null,"clearable"),i(" prop to clear combobox.")],-1),Re=b({__name:"combobox",setup(f){return(e,s)=>{const c=z,t=k,m=j,a=S,C=$,x=T,I=R;return V(),g(v,{class:"match-height"},{default:o(()=>[l(p,{cols:"12",md:"6"},{default:o(()=>[l(t,{title:"Basic",code:B},{default:o(()=>[X,l(c)]),_:1},8,["code"])]),_:1}),l(p,{cols:"12",md:"6"},{default:o(()=>[l(t,{title:"Density",code:W},{default:o(()=>[Y,l(m)]),_:1},8,["code"])]),_:1}),l(p,{cols:"12",md:"6"},{default:o(()=>[l(t,{title:"Variant",code:O},{default:o(()=>[q,l(a)]),_:1},8,["code"])]),_:1}),l(p,{cols:"12",md:"6"},{default:o(()=>[l(t,{title:"Multiple",code:F},{default:o(()=>[E,l(C)]),_:1},8,["code"])]),_:1}),l(p,{cols:"12",md:"6"},{default:o(()=>[l(t,{title:"No data with chips",code:G},{default:o(()=>[H,l(x)]),_:1},8,["code"])]),_:1}),l(p,{cols:"12",md:"6"},{default:o(()=>[l(t,{title:"Clearable",code:M},{default:o(()=>[J,l(I)]),_:1},8,["code"])]),_:1})]),_:1})}}});export{Re as default};
---
+++ assets/useAbility-BHgINCqW.js
@@ -0,0 +1,1 @@
+import{cg as i}from"./index-BkoIr6yh.js";const t=()=>i();export{t as u};
